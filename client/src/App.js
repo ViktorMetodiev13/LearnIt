7ead93b1acc9af8d91033314b6bca6f5c1c58f15
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2023-04-18 20:45:49 +0300

    react-router-dom added and made changes on css

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+import { Route, Routes } from "react-router-dom";
+
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import { Home } from "./components/Home/Home";
@@ -14,7 +16,13 @@ function App() {
             <Header />
 
             <main id="main-content">
-                <EditCourse />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/create-course" element={<CreateCourse />} />
+                    <Route path="/catalog" element={<Catalog />} />
+                </Routes>
             </main>
 
             <Footer />
